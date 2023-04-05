6a1809b23aa6e7a9a6154c823601d56cf07705df
mjtakenon <takenon777@gmail.com>
2023-04-05 22:14:01 +0900

    トップページ仮実装

--- app/app/Http/Controllers/IndexController.php
+++ app/app/Http/Controllers/IndexController.php
@@ -9,7 +9,7 @@ class IndexController extends Controller
     public function get()
     {
         $islands = Island::with(['islandStatuses' => function ($query) {
-            $query->where('turn_id', \HakoniwaService::getLatestTurn());
+            $query->where('turn_id', \HakoniwaService::getLatestTurn()->id);
         }])->whereNull('deleted_at')->get();
 
         return view('pages.index', [
--- app/resources/views/pages/index.blade.php
+++ app/resources/views/pages/index.blade.php
@@ -7,6 +7,32 @@
         <div class="home-wrapper">
             <h1 class="title"> Welcome to islands... </h1>
 
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th> 順位 </th>
+                        <th> 島 </th>
+                        <th> 発展ポイント </th>
+                        <th> 人口 </th>
+                        <th> 資金 </th>
+                        <th> 食料 </th>
+                        <th> 資源 </th>
+                    </tr>
+                </thead>
+                <tbody>
+                @foreach($islands as $island)
+                    <tr>
+                        <td> {{ 1 }} </td>
+                        <td> <a href="{{ '/islands/' . $island->id }}"> {{ $island->name }} </a> </td>
+                        <td> {{ $island->islandStatuses->first()->development_points }} </td>
+                        <td> {{ $island->islandStatuses->first()->population }} </td>
+                        <td> {{ $island->islandStatuses->first()->funds }} </td>
+                        <td> {{ $island->islandStatuses->first()->foods }} </td>
+                        <td> {{ $island->islandStatuses->first()->resources }} </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
 @endsection
