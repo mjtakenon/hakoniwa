fd3fc1bd06a5b966a4d06ee4e0d15ab42e05f60e
mjtakenon <takenon777@gmail.com>
2023-04-15 02:31:27 +0900

    噴火を実装

--- app/app/Services/Hakoniwa/Disaster/VolcanicEruption.php
+++ app/app/Services/Hakoniwa/Disaster/VolcanicEruption.php
@@ -4,15 +4,51 @@ namespace App\Services\Hakoniwa\Disaster;
 
 use App\Models\Island;
 use App\Models\Turn;
+use App\Services\Hakoniwa\Cell\Cell;
+use App\Services\Hakoniwa\Cell\Mountain;
+use App\Services\Hakoniwa\Cell\Shallow;
+use App\Services\Hakoniwa\Cell\Wasteland;
+use App\Services\Hakoniwa\Log\DestructionByVolcanicEruptionLog;
 use App\Services\Hakoniwa\Log\Logs;
+use App\Services\Hakoniwa\Log\OccurVolcanicEruptionLog;
 use App\Services\Hakoniwa\Status\Status;
 use App\Services\Hakoniwa\Terrain\Terrain;
+use App\Services\Hakoniwa\Util\Point;
+use App\Services\Hakoniwa\Util\Rand;
 
 class VolcanicEruption implements IDisaster
 {
+    const OCCUR_PROBABILITY = 0.015;
+
     public static function occur(Island $island, Terrain $terrain, Status $status, Turn $turn): DisasterResult
     {
-        // TODO: Implement occur() method.
-        return new DisasterResult($terrain, $status, Logs::create());
+        $logs = Logs::create();
+
+        if (self::OCCUR_PROBABILITY <= Rand::mt_rand_float()) {
+            return new DisasterResult($terrain, $status, $logs);
+        }
+
+        $point = new Point(5, 5);//new Point(mt_rand(0, \HakoniwaService::getMaxWidth()-1),mt_rand(0, \HakoniwaService::getMaxHeight()-1));
+
+        $terrain->setCell($point, new Mountain(point: $point));
+
+        $aroundCells = $terrain->getAroundCells($point);
+        /** @var Cell $cell */
+        foreach ($aroundCells as $cell) {
+            if ($cell::ELEVATION === -2) {
+                $logs->add(new DestructionByVolcanicEruptionLog($island, $turn, $cell));
+                $terrain->setCell($cell->getPoint(), new Shallow(point: $cell->getPoint()));
+                continue;
+            }
+            if ($cell::ELEVATION === -1 || $cell::ELEVATION === 0) {
+                $logs->add(new DestructionByVolcanicEruptionLog($island, $turn, $cell));
+                $terrain->setCell($cell->getPoint(), new Wasteland(point: $cell->getPoint()));
+                continue;
+            }
+        }
+
+        $logs->add(new OccurVolcanicEruptionLog($island, $turn, $point));
+
+        return new DisasterResult($terrain, $status, $logs);
     }
 }
---
+++ app/app/Services/Hakoniwa/Log/DestructionByVolcanicEruptionLog.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Services\Hakoniwa\Log;
+
+use App\Models\Island;
+use App\Models\Turn;
+use App\Services\Hakoniwa\Cell\Cell;
+use App\Services\Hakoniwa\Plan\Plan;
+use App\Services\Hakoniwa\Util\Point;
+
+class DestructionByVolcanicEruptionLog implements ILog
+{
+    private Island $island;
+    private Turn $turn;
+    private Cell $cell;
+
+    public function __construct(Island $island, Turn $turn, Cell $cell)
+    {
+        $this->island = $island;
+        $this->turn = $turn;
+        $this->cell = $cell;
+    }
+
+    public static function create(Island $island, Turn $turn, Cell $cell)
+    {
+        return new static($island, $turn, $cell);
+    }
+
+    public function get(): string
+    {
+        return json_encode([
+            ['text' => 'ターン ' . $this->turn->turn . ' : '],
+            ['text' => $this->island->name . '島', 'link' => '/islands/' . $this->island->id, 'style' => StyleConst::BOLD ],
+            ['text' => ' (' . $this->cell->getPoint()->x . ',' . $this->cell->getPoint()->y . ') の'],
+            ['text' => $this->cell::NAME, 'style' => StyleConst::BOLD.StyleConst::COLOR_WARNING ],
+            ['text' => 'は'],
+            $this->cell::ELEVATION < 0 ? ['text' => '海底が隆起'] : ['text' => '火砕流', 'style' => StyleConst::BOLD.StyleConst::COLOR_DANGER],
+            $this->cell::ELEVATION < 0 ? ['text' => 'しました。'] : ['text' => 'にのみこまれました。'],
+        ]);
+    }
+}
