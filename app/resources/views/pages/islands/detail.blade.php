18962d7d1aaeea1a5b914700061680ad6e003631
mjtakenon <takenon777@gmail.com>
2023-04-22 04:38:26 +0900

    島の観光をコメントアウト

--- app/resources/views/pages/islands/detail.blade.php
+++ app/resources/views/pages/islands/detail.blade.php
@@ -3,12 +3,13 @@
 @section('contents')
     <div id="app">
         @include('components.header')
-        <sightseeing-page
-            :hakoniwa="{{ $hakoniwa }}"
-            :island="{{ $island }}"
-            :island-status="{{ $islandStatus }}"
-            :island-terrain="{{ $islandTerrain->terrain }}"
-            :island-log="{{ $islandLog }}"
-        ></sightseeing-page>
+        島観光コメントアウト中
+{{--        <sightseeing-page--}}
+{{--            :hakoniwa="{{ $hakoniwa }}"--}}
+{{--            :island="{{ $island }}"--}}
+{{--            :island-status="{{ $islandStatus }}"--}}
+{{--            :island-terrain="{{ $islandTerrain->terrain }}"--}}
+{{--            :island-log="{{ $islandLog }}"--}}
+{{--        ></sightseeing-page>--}}
     </div>
 @endsection
