2a774beb9fe2920549e7e78f7d06b83f1d96fa18
mjtakenon <takenon777@gmail.com>
2023-04-23 01:59:31 +0900

    islandを表示しないよう変更

--- app/resources/views/pages/islands/detail.blade.php
+++ app/resources/views/pages/islands/detail.blade.php
@@ -5,17 +5,17 @@
         @include('components.header')
         @php
             var_dump($hakoniwa);
-            var_dump($island);
+//            var_dump($island);
             var_dump($islandStatus);
-//            var_dump($islandTerrain->terrain);
-//            var_dump($islandLog);
-        @endphp
+            var_dump($islandTerrain->terrain);
+            var_dump($islandLog);
+//        @endphp
         <sightseeing-page
             :hakoniwa="@js($hakoniwa)"
-            :island="@js($island)"
+{{--            :island="@js($island)"--}}
             :island-status="@js($islandStatus)"
-{{--            :island-terrain="@js($islandTerrain->terrain)"--}}
-{{--            :island-log="@js($islandLog)"--}}
+            :island-terrain="@js($islandTerrain->terrain)"
+            :island-log="@js($islandLog)"
         ></sightseeing-page>
     </div>
 @endsection
