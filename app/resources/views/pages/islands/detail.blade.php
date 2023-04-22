15b96c5abbfe2de9608480ba7aa4078bd2faa911
mjtakenon <takenon777@gmail.com>
2023-04-23 02:05:01 +0900

    islandterrainを表示しないようコメントアウト

--- app/resources/views/pages/islands/detail.blade.php
+++ app/resources/views/pages/islands/detail.blade.php
@@ -7,14 +7,14 @@
             var_dump($hakoniwa);
 //            var_dump($island);
             var_dump($islandStatus);
-            var_dump($islandTerrain->terrain);
+//            var_dump($islandTerrain->terrain);
             var_dump($islandLog);
 //        @endphp
         <sightseeing-page
             :hakoniwa="@js($hakoniwa)"
 {{--            :island="@js($island)"--}}
             :island-status="@js($islandStatus)"
-            :island-terrain="@js($islandTerrain->terrain)"
+{{--            :island-terrain="@js($islandTerrain->terrain)"--}}
             :island-log="@js($islandLog)"
         ></sightseeing-page>
     </div>
