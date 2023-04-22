c5d9d2398466902a5f8084886aab6cc3e2d2a2e3
mjtakenon <takenon777@gmail.com>
2023-04-23 01:54:10 +0900

    変数2つのコメントアウトを解除

--- app/resources/views/pages/islands/detail.blade.php
+++ app/resources/views/pages/islands/detail.blade.php
@@ -5,15 +5,15 @@
         @include('components.header')
         @php
             var_dump($hakoniwa);
-//            var_dump($island);
-//            var_dump($islandStatus);
+            var_dump($island);
+            var_dump($islandStatus);
 //            var_dump($islandTerrain->terrain);
 //            var_dump($islandLog);
         @endphp
         <sightseeing-page
             :hakoniwa="@js($hakoniwa)"
-{{--            :island="@js($island)"--}}
-{{--            :island-status="@js($islandStatus)"--}}
+            :island="@js($island)"
+            :island-status="@js($islandStatus)"
 {{--            :island-terrain="@js($islandTerrain->terrain)"--}}
 {{--            :island-log="@js($islandLog)"--}}
         ></sightseeing-page>
