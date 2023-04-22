e74f6eb93c0ab288835bdb284c231f028386f095
mjtakenon <takenon777@gmail.com>
2023-04-22 13:52:38 +0900

    var_dumpデバッグコード追加

--- app/resources/views/pages/islands/detail.blade.php
+++ app/resources/views/pages/islands/detail.blade.php
@@ -3,12 +3,19 @@
 @section('contents')
     <div id="app">
         @include('components.header')
-        <sightseeing-page
-            :hakoniwa="@js($hakoniwa)"
-            :island="@js($island)"
-            :island-status="@js($islandStatus)"
-            :island-terrain="@js($islandTerrain->terrain)"
-            :island-log="@js($islandLog)"
-        ></sightseeing-page>
+        @php
+            var_dump($hakoniwa);
+            var_dump($island);
+            var_dump($islandStatus);
+            var_dump($islandTerrain->terrain);
+            var_dump($islandLog);
+        @endphp
+{{--        <sightseeing-page--}}
+{{--            :hakoniwa="@js($hakoniwa)"--}}
+{{--            :island="@js($island)"--}}
+{{--            :island-status="@js($islandStatus)"--}}
+{{--            :island-terrain="@js($islandTerrain->terrain)"--}}
+{{--            :island-log="@js($islandLog)"--}}
+{{--        ></sightseeing-page>--}}
     </div>
 @endsection
