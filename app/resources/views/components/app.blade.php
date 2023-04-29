940ea6344ac93a7f5011f5d6ce66ed406300fd71
mjtakenon <takenon777@gmail.com>
2023-04-29 10:57:44 +0900

    favicon追加

--- app/resources/views/components/app.blade.php
+++ app/resources/views/components/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 {{--    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">--}}
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" type="image/x-icon" href="/favicon.ico">
     <title>@yield('title')</title>
     @if(\App::environment('local'))
         @vite(['resources/js/app.ts', 'resources/css/app.css'])
