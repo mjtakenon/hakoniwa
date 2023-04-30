50bedeb05086b562512938beb0ea55dcb5722682
rain825 <19408972+rain825@users.noreply.github.com>
2023-04-30 23:58:05 +0900

    認証手段情報を保存するテーブルを追加

---
+++ app/database/migrations/2023_04_30_231943_create_user_auths_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_auths', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('identify');
+            $table->string('provider');
+            $table->dateTime('created_at');
+            $table->dateTime('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_auths');
+    }
+};
