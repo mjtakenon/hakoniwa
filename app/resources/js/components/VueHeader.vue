291917bed721686e1eb90c619b0e577d3252e6be
Nonuplet <130939038+nonuplet@users.noreply.github.com>
2023-05-06 23:43:07 +0900

    HeaderにYahooのログインボタンを追加

--- app/resources/js/components/VueHeader.vue
+++ app/resources/js/components/VueHeader.vue
@@ -37,12 +37,16 @@
             </div>
             <div v-else class="navbar-menu">
                 <a class="navbar-item" href="/auth/google/redirect">
-                    <img src="/img/btn_google_signin_light_normal_web.png">
+                    <img class="mx-auto" src="/img/btn_google_signin_light_normal_web.png">
                 </a>
-                <a href="/auth/yahoo/redirect">
-                    <img :src="'/img/yahoo_japan_icon_64.png'">
-                    ログイン
-                </a>
+                <div class="navbar-yahoo">
+                    <a class="yahoo-link-box" href="/auth/yahoo/redirect">
+                        <div class="yahoo-logo">
+                            <img class="yahoo-logo-img" :src="'/img/yahoo_japan_icon_64.png'">
+                        </div>
+                        <span class="yahoo-text">ログイン</span>
+                    </a>
+                </div>
             </div>
         </div>
     </nav>
@@ -96,10 +100,31 @@ export default {
     }
 
     .navbar-menu {
-        @apply block max-md:mt-3 max-md:w-full md:inline-block md:ml-auto;
+        @apply block max-md:mt-3 max-md:w-full md:flex md:items-center md:ml-auto;
 
         .navbar-item {
             @apply block max-md:w-1/2 text-center mx-auto max-md:mb-2 md:inline-block md:mr-3;
+        }
+
+        .navbar-yahoo {
+            @apply block md:inline-block mx-auto w-fit bg-[#ff0033] rounded-md border-2 border-[#ff0033] drop-shadow;
+
+            .yahoo-link-box {
+                @apply flex items-center;
+
+                .yahoo-logo {
+                    @apply bg-white rounded-l;
+                }
+
+                .yahoo-logo-img {
+                    @apply w-8 mx-[11px] my-[6px];
+                }
+
+                .yahoo-text {
+                    @apply font-bold text-white px-7;
+                }
+            }
+
         }
 
         .navbar-username {
