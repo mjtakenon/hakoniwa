5660abf4fbd776888d8becfd87158dc96434997b
mjtakenon <takenon777@gmail.com>
2023-05-07 16:32:19 +0900

    ローカル環境ではviteサーバーを、ビルド環境ではmanifest.jsonからビルド内容を取るよう修正

--- app/resources/views/components/app.blade.php
+++ app/resources/views/components/app.blade.php
@@ -7,15 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="icon" type="image/x-icon" href="/favicon.ico">
     <title>@yield('title')</title>
-    @if(\App::environment('local'))
-        @vite(['resources/js/app.ts', 'resources/css/app.scss'])
-    @else
-        <link rel="manifest" href="/build/manifest.json"/>
-        <script type="module" src="/build/assets/bundle.js"></script>
-        <script type="module" src="/build/assets/bundle2.js"></script>
-        <link rel="stylesheet" href="/build/assets/asset.css"/>
-{{--        <script src="/build/assets/assets.js"></script>--}}
-    @endif
+    @vite(['resources/js/app.ts', 'resources/css/app.scss'])
 </head>
 <body>
     <div>
