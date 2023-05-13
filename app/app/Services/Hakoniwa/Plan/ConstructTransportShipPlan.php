563e00aafc688267daee4a588dc0c5882eb1492a
mjtakenon <takenon777@gmail.com>
2023-05-14 04:46:26 +0900

    輸送船建造の回数指定を追加

--- app/app/Services/Hakoniwa/Plan/ConstructTransportShipPlan.php
+++ app/app/Services/Hakoniwa/Plan/ConstructTransportShipPlan.php
@@ -24,15 +24,22 @@ class ConstructTransportShipPlan extends Plan
     public const PRICE = 50;
     public const PRICE_STRING = '(' . self::PRICE . '億円)';
     public const USE_POINT = false;
+    public const USE_AMOUNT = true;
+    public const AMOUNT_STRING = '(:amount:回実施)';
+    public const DEFAULT_AMOUNT_STRING = '(1回実施)';
 
     protected string $key = self::KEY;
     protected string $name = self::NAME;
     protected int $price = self::PRICE;
     protected bool $usePoint = self::USE_POINT;
+    protected bool $useAmount = self::USE_AMOUNT;
+    protected string $amountString = self::AMOUNT_STRING;
+    protected string $defaultAmountString = self::DEFAULT_AMOUNT_STRING;
 
     public function execute(Island $island, Terrain $terrain, Status $status, Turn $turn, Collection $foreignIslandTargetedPlans): ExecutePlanResult
     {
         if ($status->getFunds() < self::PRICE) {
+            $this->amount = 0;
             $logs = Logs::create()->add(new AbortLackOfFundsLog($island, $turn, $this->point, $this));
             return new ExecutePlanResult($terrain, $status, $logs, false);
         }
@@ -43,9 +50,12 @@ class ConstructTransportShipPlan extends Plan
         });
 
         if ($seaCells->count() <= 0) {
+            $this->amount = 0;
             $logs = Logs::create()->add(new AbortInvalidTerrainLog($island, $turn, $this));
             return new ExecutePlanResult($terrain, $status, $logs, false);
         }
+
+        $this->amount -= 1;
 
         /** @var Cell $cell */
         $cell = $seaCells->random();
