4fc43e199af0e3bb1b30c97d5325eb6ebe0f4c41
Nonuplet <130939038+nonuplet@users.noreply.github.com>
2023-05-14 20:12:04 +0900

    仮でフロント側にパーサーを実装、新しいデスクトップ表示の仮実装

--- app/resources/js/components/LogViewer.vue
+++ app/resources/js/components/LogViewer.vue
@@ -13,16 +13,113 @@
                 </span>
             </span>
         </div>
+
+        <div class="subtitle">
+            {{ store.island.name }}島の近況
+        </div>
+        <div class="log-texts flex items-start" v-for="(log, logIndex) of logs" :key="'log-' + logIndex">
+
+            <div class="my-1 border-r border-gray-300">
+                <div class="w-fit mr-2 text-center">
+                    <span class="block text-gray-500 text-xs">ターン</span>
+                    <span class="block -mt-1 font-bold text-lg">{{log.turn}}</span>
+                </div>
+            </div>
+            <div class="grow">
+                <div
+                    v-for="(line, lineIndex) of log.texts"
+                    :key="'line-' + logIndex + '-' + lineIndex"
+                    class="pl-2"
+                >
+                    <span>・</span>
+                    <template
+                        v-for="(context, conIndex) of line"
+                        :key="'text-' + logIndex + '-' + lineIndex + '-' + conIndex"
+                    >
+                        <a v-if="context.hasOwnProperty('link')" :href="context.link" :style="context.style">
+                            {{context.text}}
+                        </a>
+                        <span v-else :style="context.style">
+                            {{context.text}}
+                        </span>
+                    </template>
+                </div>
+                <div class="mt-auto grid grid-cols-5 gap-2 pl-[20vw] text-left">
+                    <div class="w-full flex items-center px-0.5 border-b-2" :class="getBgColor(log.diff.foods)">
+                        <span class="gray-600 text-[6px] mr-1">🍎食料</span>
+                        <span class="text-sm font-bold grow text-right" :class="getTextColor(log.diff.foods)">
+                            {{ log.diff.foods > 0 ? "+" + log.diff.foods : log.diff.foods}}
+                        </span>
+                        <span class="gray-600 text-[6px]">㌧</span>
+                    </div>
+
+                    <div class="w-full flex items-center px-0.5 border-b-2" :class="getBgColor(log.diff.funds)">
+                        <span class="gray-600 text-[6px] mr-1">💰資金</span>
+                        <span class="text-sm font-bold grow text-right" :class="getTextColor(log.diff.funds)">
+                            {{ log.diff.funds > 0 ? "+" + log.diff.funds : log.diff.funds}}
+                        </span>
+                        <span class="gray-600 text-[6px]">億円</span>
+                    </div>
+
+                    <div class="w-full flex items-center px-0.5 border-b-2" :class="getBgColor(log.diff.resources)">
+                        <span class="gray-600 text-[6px] mr-1">🏭資源</span>
+                        <span class="text-sm font-bold grow text-right" :class="getTextColor(log.diff.resources)">
+                            {{ log.diff.resources > 0 ? "+" + log.diff.resources : log.diff.resources}}
+                        </span>
+                        <span class="gray-600 text-[6px]">㌧</span>
+                    </div>
+
+                    <div class="w-full flex items-center px-0.5 border-b-2" :class="getBgColor(log.diff.population)">
+                        <span class="gray-600 text-[6px] mr-1">👤人口</span>
+                        <span class="text-sm font-bold grow text-right" :class="getTextColor(log.diff.population)">
+                            {{ log.diff.population > 0 ? "+" + log.diff.population : log.diff.population}}
+                        </span>
+                        <span class="gray-600 text-[6px]">人</span>
+                    </div>
+
+                    <div class="w-full flex items-center px-0.5 border-b-2" :class="getBgColor(log.diff.points)">
+                        <span class="gray-600 text-[6px] mr-1">⚡ポイント</span>
+                        <span class="text-sm font-bold grow text-right" :class="getTextColor(log.diff.points)">
+                            {{ log.diff.points > 0 ? "+" + log.diff.points : log.diff.points}}
+                        </span>
+                        <span class="gray-600 text-[6px]">pts</span>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 <script lang="ts">
 import {defineComponent} from "vue";
 import {useMainStore} from "../store/MainStore";
+import {LogParser, NewLog} from "../store/Entity/NewLog";
 
 export default defineComponent({
+    data() {
+        return {
+            logs: [] as NewLog[]
+        }
+    },
     setup() {
         const store = useMainStore();
         return { store };
+    },
+    mounted() {
+        const logs = this.store.logs;
+        const parser = new LogParser();
+        this.logs = parser.parse(logs);
+    },
+    methods: {
+        getBgColor(num: number): string {
+            if (num > 0) return 'border-blue-200'
+            if (num === 0) return 'border-gray-300'
+            return 'border-red-200'
+        },
+        getTextColor(num: number): string {
+            if (num > 0) return 'text-blue-600'
+            if (num === 0) return 'text-black'
+            return 'text-red-600'
+        }
     }
 });
 </script>
