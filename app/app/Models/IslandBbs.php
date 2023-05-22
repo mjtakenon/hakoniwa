7cc9fa51a972a365bbb00ab106639c82bfb4c0d0
Nonuplet <130939038+nonuplet@users.noreply.github.com>
2023-05-22 11:02:32 +0900

    ide-helper周りの改善

--- app/app/Models/IslandBbs.php
+++ app/app/Models/IslandBbs.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @mixin IdeHelperIslandBbs
+ */
 class IslandBbs extends Model
 {
     use HasFactory;
--- app/app/Models/IslandComment.php
+++ app/app/Models/IslandComment.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * @mixin IdeHelperIslandComment
+ */
 class IslandComment extends Model
 {
     use SoftDeletes;
