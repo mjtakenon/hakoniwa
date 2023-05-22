c83aef5739cd987de6f0167943fe16df8e587129
mjtakenon <takenon777@gmail.com>
2023-05-22 22:02:07 +0900

    ユーザー設定画面のコントローラー実装

---
+++ app/app/Http/Controllers/Web/Settings/IndexController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Web\Settings;
+
+use App\Http\Controllers\Controller;
+use App\Models\Island;
+use App\Models\Turn;
+
+class IndexController extends Controller
+{
+    public function get()
+    {
+        // ログインしているが島を保有していない場合は、島登録ページに飛ばす
+        if (!\HakoniwaService::isIslandRegistered()) {
+            return redirect()->route('register');
+        }
+
+        $island = Island::find(\Auth::user()->island->id);
+
+        $turn = Turn::latest()->firstOrFail();
+
+        $islandStatus = $island->islandStatuses->where('turn_id', $turn->id)->firstOrFail();
+
+//        return view('pages.settings', [
+        return response()->json([
+            'island' => [
+                'id' => $island->id,
+                'name' => $island->name,
+                'owner_name' => $island->owner_name,
+                'status' => [
+                    'development_points' => $islandStatus->development_points,
+                    'funds' => $islandStatus->funds,
+                    'foods' => $islandStatus->foods,
+                    'resources' => $islandStatus->resources,
+                    'population' => $islandStatus->population,
+                    'funds_production_capacity' => $islandStatus->funds_production_capacity,
+                    'foods_production_capacity' => $islandStatus->foods_production_capacity,
+                    'resources_production_capacity' => $islandStatus->resources_production_capacity,
+                    'environment' => $islandStatus->environment,
+                    'area' => $islandStatus->area,
+                ],
+            ]
+        ]);
+    }
+}
