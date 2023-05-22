dfc535f9a674ea492e4de0cba8adfb84bfa7e664
mjtakenon <takenon777@gmail.com>
2023-05-22 22:54:39 +0900

    メンテナンス画面を実装

--- app/routes/web.php
+++ app/routes/web.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 $baseMiddleware = [
+    \App\Http\Middleware\MaintenanceFilter::class,
     \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
     \Illuminate\Session\Middleware\StartSession::class,
     \Illuminate\View\Middleware\ShareErrorsFromSession::class,
