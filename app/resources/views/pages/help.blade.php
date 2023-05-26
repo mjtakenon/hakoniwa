c1dce67b3cb024acdb3cbe1fe9289fc11cc02918
mjtakenon <takenon777@gmail.com>
2023-05-26 20:43:55 +0900

    helpとprivacyのBladeを仮設置

---
+++ app/resources/views/pages/help.blade.php
@@ -0,0 +1,9 @@
+@extends('components.app')
+@section('title', 'やまにてぃ（仮）')
+@section('contents')
+    <div id="app">
+        @include('components.header')
+        工事中です。👷🏗
+        @include('components.footer')
+    </div>
+@endsection
