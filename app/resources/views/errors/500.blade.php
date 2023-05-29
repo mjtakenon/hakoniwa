7257020fd215519fed6563a0f06c0ab542fc1dec
mjtakenon <takenon777@gmail.com>
2023-05-29 22:29:04 +0900

    簡易的に404, 500エラーページを実装

---
+++ app/resources/views/errors/500.blade.php
@@ -0,0 +1,12 @@
+@extends('components.app')
+@section('title', 'やまにてぃ（仮）')
+@section('contents')
+    <div id="app">
+        @include('components.header')
+        <div id="index">
+            <div class="subtitle">500 Internal Server Error</div>
+            <div>サーバーエラーが発生しました。しばらく経っても改善しない場合、管理者にお問い合わせください。</div>
+            <a href="/">HOMEへ戻る</a>
+        </div>
+    </div>
+@endsection
