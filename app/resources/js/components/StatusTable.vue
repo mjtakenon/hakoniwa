e6637633bbbcd6a58b5f21045740f366339e195b
Nonuplet <130939038+nonuplet@users.noreply.github.com>
2023-06-20 15:30:52 +0900

    デスクトップ表示の実装

--- app/resources/js/components/StatusTable.vue
+++ app/resources/js/components/StatusTable.vue
@@ -1,46 +1,126 @@
 <template>
     <div class="stats">
-        <div
-            v-for="(status, index) in statuses"
-            class="stats-box"
-            :class="{'max-md:col-span-3': (index === 0)}"
-            :key="status.title"
-        >
-            <div class="stats-box-title">
-                {{ status.title }}
+        <div class="stats-header">
+            <div class="names">
+                <h1 class="island-name">{{store.island.name}}島</h1>
+                <span class="owner-name">({{store.island.owner_name}})</span>
             </div>
-            <div class="stats-box-data">
-                <div
-                    class="stats-box-num-wrapper"
-                    :id="'data-num-' + index"
-                >
-                    <div
-                        class="stats-box-data-num"
-                        :style="this.isMobile ?
-                            {fontSize: 'clamp(1px,'+ status.fontSize +'px,1.05rem)'} :
-                            {fontSize: 'clamp(1px,'+ status.fontSize +'px,1.2rem)'}
-                        "
-                    >
-                        {{ status.numText }}
+            <div class="header-achievements">
+                <achievement-icons :achievement_data="store.achievements"></achievement-icons>
+            </div>
+        </div>
+        <div class="stats-contents">
+            <div class="stats-island">
+                <div class="data-point stat-box-island border-b">
+                    <div class="stat-box-title">発展ポイント</div>
+                    <div class="stat-inner">
+                        <div class="stat-box-num">
+                            {{store.status.development_points.toLocaleString()}}
+                        </div>
+                        <div class="stat-box-unit">
+                            pts
+                        </div>
                     </div>
                 </div>
-                <div class="stat-box-data-unit">
-                    {{ status.unit }}
+                <div class="data-area stat-box-island border-b">
+                    <div class="stat-box-title">面積</div>
+                    <div class="stat-inner">
+                        <div class="stat-box-num">
+                            {{store.status.area.toLocaleString()}}
+                        </div>
+                        <div class="stat-box-unit">
+                            万坪
+                        </div>
+                    </div>
+                </div>
+                <div class="data-environment stat-box-island border-b">
+                    <div class="stat-box-title">環境</div>
+                    <div class="stat-inner">
+                        <div class="stat-box-num">
+                            {{store.getEnvironmentString}}
+                        </div>
+                        <div class="stat-box-unit">
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="stats-box achievements-box">
-            <div class="stats-box-title">
-                実績
-            </div>
-            <achievement-icons class="achievement-icons" :achievement_data="store.achievements"></achievement-icons>
-        </div>
-        <div class="stats-box comment-box">
-            <div class="stats-box-title">
-                コメント
-            </div>
-            <div class="island-comment" :class="{'text-sm text-on-surface-variant': hasComment}">
-                {{ islandComment }}
+            <div class="stats-info">
+                <div class="stats-summary">
+                    <div class="stats-subtitle">島の情報</div>
+                    <div class="stat-box-info">
+                        <font-awesome-icon icon="fa-solid fa-sack-dollar" class="stat-box-icon" />
+                        <div class="stat-box-title">資金</div>
+                        <div class="stat-box-num">{{store.status.funds.toLocaleString()}}</div>
+                        <div class="stat-box-unit">億円</div>
+                    </div>
+                    <div class="stat-box-info">
+                        <font-awesome-icon icon="fa-solid fa-wheat-awn" class="stat-box-icon" />
+                        <div class="stat-box-title">食料</div>
+                        <div class="stat-box-num">{{store.status.foods.toLocaleString()}}</div>
+                        <div class="stat-box-unit">㌧</div>
+                    </div>
+                    <div class="stat-box-info">
+                        <font-awesome-icon icon="fa-solid fa-oil-well" class="stat-box-icon" />
+                        <div class="stat-box-title">資源</div>
+                        <div class="stat-box-num">{{store.status.resources.toLocaleString()}}</div>
+                        <div class="stat-box-unit">㌧</div>
+                    </div>
+                </div>
+                <div class="stats-human">
+                    <div class="stats-subtitle">人的資源</div>
+                    <div class="stats-human-inner">
+                        <div class="stats-human-left">
+                            <div class="stat-box-human-left population">
+                                <div class="stat-box-title">総人口</div>
+                                <div class="stat-inner">
+                                    <div class="stat-box-num">
+                                        {{store.status.development_points.toLocaleString()}}
+                                    </div>
+                                    <div class="stat-box-unit">
+                                        人
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="stat-box-human-left unassigned">
+                                <div class="stat-box-title">未割当</div>
+                                <div class="stat-inner">
+                                    <div class="stat-box-num">
+                                        TODO
+                                    </div>
+                                    <div class="stat-box-unit">
+                                        人
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="stats-human-right">
+                            <div class="stat-box-info">
+                                <font-awesome-icon icon="fa-solid fa-wheat-awn" class="stat-box-icon" />
+                                <div class="stat-box-title">農業</div>
+                                <div class="stat-box-num">{{store.status.foods_production_capacity.toLocaleString()}}</div>
+                                <div class="stat-box-unit">人</div>
+                            </div>
+                            <div class="stat-box-info">
+                                <font-awesome-icon icon="fa-solid fa-sack-dollar" class="stat-box-icon" />
+                                <div class="stat-box-title">工業</div>
+                                <div class="stat-box-num">{{store.status.funds_production_capacity.toLocaleString()}}</div>
+                                <div class="stat-box-unit">人</div>
+                            </div>
+                            <div class="stat-box-info">
+                                <font-awesome-icon icon="fa-solid fa-oil-well" class="stat-box-icon" />
+                                <div class="stat-box-title">資源生産</div>
+                                <div class="stat-box-num">{{store.status.resources_production_capacity.toLocaleString()}}</div>
+                                <div class="stat-box-unit">人</div>
+                            </div>
+                            <div class="stat-box-info">
+                                <font-awesome-icon icon="fa-solid fa-shield" class="stat-box-icon"/>
+                                <div class="stat-box-title">軍事</div>
+                                <div class="stat-box-num">TODO</div>
+                                <div class="stat-box-unit">人</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -51,6 +131,8 @@ import {defineComponent} from "vue";
 import {useMainStore} from "../store/MainStore";
 import AchievementIcons from "./AchievementIcons.vue";
 import {storeToRefs} from "pinia";
+import {library} from "@fortawesome/fontawesome-svg-core";
+import {faOilWell, faSackDollar, faShield, faWheatAwn} from "@fortawesome/free-solid-svg-icons";
 
 export default defineComponent({
     components: {
@@ -69,6 +151,8 @@ export default defineComponent({
         }
     },
     setup() {
+        library.add(faSackDollar,faWheatAwn,faOilWell, faShield)
+
         const store = useMainStore();
         const {status: statusRef} = storeToRefs(store);
         return {store, statusRef};
@@ -193,45 +277,149 @@ export default defineComponent({
 
 <style lang="scss" scoped>
 .stats {
-    @apply container px-2 py-1 md:mx-auto mb-3 md:mb-6 max-w-full grid grid-cols-3 md:grid-cols-5 gap-2;
+    @apply container mx-auto w-full bg-surface text-on-surface drop-shadow-md rounded-lg mb-4;
+    @apply md:py-2 md:px-4;
 
-    .stats-box {
-        @apply bg-surface-variant rounded-xl p-2 drop-shadow-md;
+    .stats-header {
+        @apply w-full flex py-3 mb-4 border-b-2 border-dashed;
 
-        .stats-box-title {
-            @apply font-bold text-left text-on-surface-variant text-xs lg:text-sm;
+        .names {
+            @apply grow flex items-end justify-center min-w-0;
+
+            .island-name {
+                @apply font-bold text-2xl mr-2;
+            }
+
+            .owner-name {
+                @apply text-on-surface-variant text-sm;
+            }
         }
 
-        .stats-box-data {
-            @apply flex items-end flex-wrap h-8;
-
-            .stats-box-num-wrapper {
-                @apply max-lg:w-full lg:grow min-w-0 text-right;
-            }
-
-            .stats-box-data-num {
-                @apply font-bold w-full inline;
-            }
-
-            .stat-box-data-unit {
-                @apply max-lg:w-full max-lg:-mt-1.5 text-[0.6rem] lg:text-sm text-right lg:pl-2;
-            }
+        .header-achievements {
+            @apply w-full;
+            @apply md:w-fit md:border-l md:min-w-[20%];
         }
     }
 
-    .achievements-box {
-        @apply col-span-3 md:col-span-2 z-10;
+    .stats-contents {
+        @apply w-full;
 
-        .achievement-icons {
-            @apply w-fit max-w-full mx-auto;
+        .stat-box-island {
+            @apply py-1;
+
+            .stat-box-title {
+                @apply text-on-surface-variant text-sm text-left font-bold leading-none;
+            }
+            .stat-inner {
+                @apply flex items-end;
+
+                .stat-box-num {
+                    @apply grow min-w-0 text-on-surface text-right font-bold text-xl leading-none;
+                }
+                .stat-box-unit {
+                    @apply ml-2 text-on-surface-variant leading-none;
+                }
+            }
         }
-    }
 
-    .comment-box {
-        @apply col-span-3;
+        .stat-box-info {
+            @apply flex items-center gap-2 py-1 px-2 rounded-lg bg-surface-variant text-on-surface-variant mb-1.5;
 
-        .island-comment {
-            @apply px-2 md:px-4 my-1 text-left leading-none;
+            .stat-box-icon {
+                @apply text-xl;
+            }
+
+            .stat-box-title {
+                @apply text-sm font-bold;
+            }
+
+            .stat-box-num {
+                @apply grow min-w-0 text-right font-bold text-xl;
+            }
+
+            .stat-box-unit {
+                @apply mt-auto text-xs text-right w-[24px];
+            }
+        }
+
+        .stat-box-human-left {
+            @apply flex flex-wrap py-1 px-2 w-full grow rounded-lg mb-2;
+
+            .stat-box-title {
+                @apply w-full text-on-surface-variant text-sm text-left font-bold leading-none;
+            }
+            .stat-inner {
+                @apply flex items-end w-full pb-2;
+
+                .stat-box-num {
+                    @apply grow min-w-0 text-on-surface text-right font-bold text-xl leading-none;
+                }
+                .stat-box-unit {
+                    @apply ml-2 text-on-surface-variant leading-none;
+                }
+            }
+
+            &.population {
+                @apply bg-primary;
+
+                .stat-box-title,.stat-box-unit {
+                    @apply text-on-primary;
+                }
+                .stat-box-num {
+                    @apply text-surface;
+                }
+            }
+
+            &.unassigned {
+                @apply bg-primary-container;
+
+                .stat-box-title,.stat-box-unit {
+                    @apply text-on-primary-container;
+                }
+                .stat-box-num {
+                    @apply text-on-surface;
+                }
+            }
+        }
+
+        .stats-island {
+            @apply flex mb-6;
+            @apply md:gap-8;
+
+            .data-point {
+                @apply w-1/2;
+            }
+            .data-area,.data-environment {
+                @apply w-1/4;
+            }
+        }
+
+        .stats-info {
+            @apply flex;
+            @apply md:gap-10;
+
+            .stats-subtitle {
+                @apply text-left font-bold text-on-surface-variant text-sm;
+            }
+
+            .stats-summary {
+                @apply w-1/3;
+            }
+            .stats-human {
+                @apply w-2/3;
+
+                .stats-human-inner {
+                    @apply flex gap-4 items-stretch;
+
+                    .stats-human-left {
+                        @apply flex flex-wrap
+                    }
+
+                    .stats-human-left,.stats-human-right {
+                        @apply w-1/2;
+                    }
+                }
+            }
         }
     }
 }
