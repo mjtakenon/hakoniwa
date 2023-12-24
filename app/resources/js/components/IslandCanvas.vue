cf0f084bfe42cf700ddf6886aa673eb24e1cd262
mjtakenon <takenon777@gmail.com>
2023-12-24 14:41:33 +0900

    カメラの制限を追加

--- app/resources/js/components/IslandCanvas.vue
+++ app/resources/js/components/IslandCanvas.vue
@@ -1,28 +1,30 @@
 <template>
-    <TresPerspectiveCamera
-        ref="camera"
-        :position="[64, 192, 192] as Vector3"
-        :look-at="[0, 0, 0] as Vector3"
-    />
-    <CameraControls
-        make-default
-    />
-    <TresGroup :position="[-64, 0, -64] as Vector3">
-        <template v-for="terrain of store.terrains">
-            <Suspense>
-                <IslandCell
-                    :position="[terrain.data.point.x*8+((terrain.data.point.y%2-1)*4), 0, terrain.data.point.y*8] as Vector3"
-                    :terrain="terrain"
-                ></IslandCell>
-            </Suspense>
-        </template>
-    </TresGroup>
+    <TresGroup>
+        <TresPerspectiveCamera
+            ref="camera"
+            :position="[64, 192, 192] as Vector3"
+        />
+        <CameraControls
+            v-bind="cameraControlsState"
+            make-default
+        />
+        <TresGroup :position="[-64, 0, -64] as Vector3">
+            <template v-for="terrain of store.terrains">
+                <Suspense>
+                    <IslandCell
+                        :position="[terrain.data.point.x*8+((terrain.data.point.y%2-1)*4), 0, terrain.data.point.y*8] as Vector3"
+                        :terrain="terrain"
+                    ></IslandCell>
+                </Suspense>
+            </template>
+        </TresGroup>
 
-    <TresAmbientLight :intensity="2"/>
-    <TresDirectionalLight
-        :position="[192, 192, 192] as Vector3"
-        :intensity="3"
-    />
+        <TresAmbientLight :intensity="2"/>
+        <TresDirectionalLight
+            :position="[192, 192, 192] as Vector3"
+            :intensity="3"
+        />
+    </TresGroup>
 </template>
 
 <script setup lang="ts">
@@ -38,17 +40,24 @@ import {
 import {BasicShadowMap, NoToneMapping, Raycaster, SRGBColorSpace, Vector2, Vector3} from 'three'
 import {CameraControls, Box} from '@tresjs/cientos'
 
-import {ref, shallowReactive, shallowRef, ShallowRef, watchEffect} from 'vue'
+import {reactive, ref, shallowReactive, shallowRef, ShallowRef, watchEffect} from 'vue'
 import {useGLTF, OrbitControls} from '@tresjs/cientos'
 import {Terrain} from "../store/Entity/Terrain"
 import {useMainStore} from "../store/MainStore"
 import {storeToRefs} from "pinia"
 import IslandCell from "./IslandCell.vue"
 
-const objectRef: ShallowRef<TresInstance | null> = shallowRef(null)
-const camera = ref(null)
-const group = ref(null)
-const canvas = ref(null)
+// const camera1 = ref(null)
+// const camera2 = ref(null)
+// const group = ref(null)
+// const canvas = ref(null)
+let controls = null
+
+const cameraControlsState = reactive({
+    minDistance: 20,
+    maxDistance: 200,
+    maxPolarAngle: Math.PI / 2,
+})
 
 const store = useMainStore()
 
