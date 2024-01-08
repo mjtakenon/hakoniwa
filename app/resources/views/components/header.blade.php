b8843e52209fc16cd0c6ec5c028eb7a9ee70aa0c
Kokonoe <130939038+nonuplet@users.noreply.github.com>
2024-01-08 14:12:59 +0900

    DEBUG_LOGIN_USING_ID未定義時に空文字列をpropsに渡す問題の修正
    development環境の時、.envにDEBUG_LOGIN_USING_IDが定義されていないとconfigの取得値がnullになってしまい、結果としてpropsに空文字列が渡されてしまいVueがWarningを吐いてしまいます。
    Blede側でnullチェックを行い、値が存在しない場合はpropsを渡さないように修正しました。

--- app/resources/views/components/header.blade.php
+++ app/resources/views/components/header.blade.php
@@ -12,7 +12,10 @@
     ></vue-header>
     @if(\App::environment('local') && \APP::hasDebugModeEnabled() && file_exists(public_path('hot')))
         <debug-tools
-            :debug-login-using-id="{{ config('app.hakoniwa.debug.login_using_id') }}"
+            @php($debug_login_id = config('app.hakoniwa.debug.login_using_id'))
+            @if($debug_login_id !== null)
+                :debug-login-using-id="{{ $debug_login_id }}"
+            @endif
         ></debug-tools>
     @endif
 </header>
